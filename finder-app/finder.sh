64820fd1fa2887afb791cd4b5061da0cb646a0d4
Alexle <Alexle@kaonbroadband.com>
2026-09-22 15:15:18 +0700

    Merge conflict resolved

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash 
-
-filesdir=$1
-searchstr=$2
-
-#validate argument count
-if [ $# -ne 2 ]; then   
-    echo "Error: expected 2 arguments: <filesdir> <searchstr>"
-    exit 1
-fi
-
-#validate filesdir is a real directory
-
-if [ ! -d "$filesdir" ]; then
-    echo "Error: $filesdir is not a directory"
-    exit 1
-fi
-numfiles=$(find "$filesdir" -type f | wc -l)
-numlines=$(grep -r "$searchstr" "$filesdir" | wc -l)
-
-echo "The number of files are $numfiles and the number of matching lines are $numlines"
-=======
 #!/bin/bash
 
 if [ $# -ne 2 ]; then 
@@ -40,4 +17,3 @@ fi
 file_count=$(find "$filedir" -type f| wc -l)
 matching_lines=$(grep -r "$searchstr" "$filedir" | wc -l)
 echo "The number of files are $file_count and the number of matching lines are $matching_lines"
->>>>>>> d0d984852415bc7b9f2bb2a67ec8bb385e8bfa75
--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,36 +6,36 @@
 
 int main(int argc, char *argv[]){
     openlog("writer", LOG_PID, LOG_USER);
-    if (argc != 3){
-        syslog(LOG_ERR, "Invalid number of arguments: %d", argc);
-        fprintf(stderr, "Error: Two parameters required\n");
-        fprintf(stderr, "Usage: %s <writefile> <writestr>\n", argv[0]);
+
+    if (argc < 3){
+        syslog(LOG_ERR, "Usage: <writefile> <writestr>");
+        fprintf(stderr, "Error: 2 arguments required.\n");
         closelog();
         return 1;
     }
+
     const char *writefile = argv[1];
     const char *writestr = argv[2];
+
+    FILE *fp = fopen(writefile, "wb");
+    if (fp == NULL){
+        syslog(LOG_ERR, "Could not open %s: %s", writefile, strerror(errno));
+        closelog();
+        return 1;
+    }
+
+    size_t len = strlen(writestr);
+    size_t written = fwrite(writestr, 1, len, fp);
+
+    if (written != len){
+        syslog(LOG_ERR, "Failed to write full string to %s", writefile);
+        fclose(fp);
+        closelog();
+        return 1;
+    }
     syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
-    FILE *file = fopen(writefile, "w");
-    if (file == NULL){
-        syslog(LOG_ERR, "Could not open file %s for writing: %s", writefile, writestr);
-        fprintf(stderr, "Error: Could not create file %s", writefile);
-        closelog();
-        return 1;
-    }
-    if (fprintf(file, "%s", writestr) < 0){
-        syslog(LOG_ERR, "Error writing to file %s: %s", writefile, strerror(errno));
-        fprintf(stderr, "Error: Could not write to file %s\n", writefile);
-        fclose(file);
-        closelog();
-        return 1;
-    }
-    if (fclose(file) !=0){
-        syslog(LOG_ERR, "Error closing file %s: %s", writefile, strerror(errno));
-        fprintf(stderr, "Error: Could not close file %s\n", writefile);
-        closelog();
-        return 1;
-    }
+
+    fclose(fp);
     closelog();
     return 0;
 
